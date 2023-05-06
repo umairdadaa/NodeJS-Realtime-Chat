c34ef41d2042b5b8722fdfd28e8f75436d2f574d
Umair Dada <75429136+umairdadaa@users.noreply.github.com>
2023-05-06 16:35:32 +0400

    Backend #1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NodeJS Realtime Chat
  https://www.youtube.com/watch?v=Fzv-rgwcFKk
+
+## Libraries Used
+ - Express
+ - Axios
+ - Cors
+ - Nodemon
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,24 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+
+  try {
+    const r = await axios.put("https://api.chatengine.io/users/", {
+        username: username,
+        secret: username,
+        first_name: username,
+    }, { headers: { "private-key": "00a94592-1c36-4376-9bfc-ef02459ebaa4" } })
+    return res.status(r.status).json(r.data) }
+    catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
+
+// Project ID:
+// b03793ff-74b3-4a4b-9212-ecc308096c4f
+// Private Key:
+// 00a94592-1c36-4376-9bfc-ef02459ebaa4
+
 
 app.listen(3001);
